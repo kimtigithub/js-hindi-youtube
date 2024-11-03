9df9aa197b5b54c8cf0d1830a6485de6a9907020
kimti github <kanvisark@gmail.com>
2024-11-03 21:39:17 +0700

    some changes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,24 +1,54 @@
-let score = true
+// let score = true
 
 //console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN not a number but conversion number
 // true => 1;
 //false => 0;
 
-let isLoggedIn = 0 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+//let isLoggedIn = 0 
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
 
 // 1=> true , 0 => false;
 //"" => false , "Kimti" => true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//let stringNumber = String(someNumber)
+//console.log(typeof stringNumber)
+
+//Operations-------------
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2**2); //2^2
+
+// let str1 = 'HELLO'
+// let str2 = "Kimti"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2 + 2) //122 depend kore first e string nibo na number .. jodi string sob sevabe count kore
+// console.log(1 + 2 + "2") //32  r jodi number nei number age count kore then string hobe
+
+
+//console.log(+true) // 1
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+// console.log(num1)
+
+let gameCounter = 100
+gameCounter;
+console.log(gameCounter)
