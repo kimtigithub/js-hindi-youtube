ac50fed2df44dd6aa07a959057de0d2cf4cf6f5e
kimti github <kanvisark@gmail.com>
2024-11-03 21:50:40 +0700

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2" > 1);
+console.log(null == 0); //false cause null is a empty value
+console.log(null > 0);//false comparisons convert null to a number, treating it as 0, that's why false
+console.log(null >= 0);//true that's why true
+
+console.log(undefined == 0);
+
+// ===  chcke values strictly
+console.log("2" === 2); // avoid all of these things in coding 
