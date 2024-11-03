504911e5857014334a41190e69477b392b3ab8b8
kimti github <kanvisark@gmail.com>
2024-11-03 21:06:08 +0700

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = true
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN not a number but conversion number
+// true => 1;
+//false => 0;
+
+let isLoggedIn = 0 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1=> true , 0 => false;
+//"" => false , "Kimti" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
