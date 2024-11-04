a38d382c0703c135225dab35cd176b7732a10c03
kimti github <kanvisark@gmail.com>
2024-11-04 17:49:41 +0700

    maths and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //two show precision value .00 like this ..
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));//3 mane tin porjntw rekhe baki kete dibe..
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); // beshi boro value k comma diye seperate kore
+
+//Maths++++++++++++++++++++
+//Maths library come in javascript by default
+
+console.log(Math);
+console.log(Math.abs(-5)); //absolute value ..output 5
+console.log(Math.round(4.3)); //4 mane .3 bad jabe
+console.log(Math.ceil(4.2));//upper value nibe
+console.log(Math.floor(4.5));//lower value
+console.log(Math.min(1,2,3,4,5));
+
+console.log(Math.random());//always values come between 0 to 1
+console.log(Math.floor(Math.random()*10 )+ 1); //+1 mean to avoid 0 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) //*** Formula 
+
+
